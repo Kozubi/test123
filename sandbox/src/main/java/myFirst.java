a0add730fd9fb2bb53948b83fd494575f3df17de
Marcin Kozubała <MarcinKo@inns.local>
2018-04-25 10:54:24 +0200

    test 123

--- sandbox/src/main/java/myFirst.java
+++ sandbox/src/main/java/myFirst.java
@@ -4,6 +4,8 @@ public class myFirst {
     public static void main(String[] args) {
 
         System.out.println("Hello world java start");
+        System.out.println("Seima");
+        
 
     }
 }
