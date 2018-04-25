b24628bdeecbfbd7b1d02e8f25069730d317bdaf
Marcin Kozubała <MarcinKo@inns.local>
2018-04-25 15:11:57 +0200

    b

--- Point.java
+++ Point.java
@@ -18,7 +18,6 @@ public class Point{
     public static void main(String[] args){
         Point p1 = new Point(0, 0);
         Point p2 = new Point(2345, 211);
-        
         double dst = p1.distance(p1, p2);
 
         System.out.println("Punkt1");
